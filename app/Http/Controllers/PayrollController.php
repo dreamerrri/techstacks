3366ff80e8e67623727beb538cbcc64a71a1094f
Vincent Cruz <vincentcruz54@gmail.com>
2026-05-29 16:24:56 +0800

    ADDED SORTING FOR PAYROLL SUMMARY TABLE

--- app/Http/Controllers/PayrollController.php
+++ app/Http/Controllers/PayrollController.php
@@ -49,7 +49,10 @@ class PayrollController extends Controller
             $query->where('department', $request->department);
         }
 
-        $employees = $query->orderBy('last_name')->paginate(15)->withQueryString();
+        $employees = $query->orderByRaw('CASE WHEN EXISTS (SELECT 1 FROM attendances WHERE attendances.employee_id = employees.id) THEN 0 ELSE 1 END')
+            ->orderBy('last_name')
+            ->paginate(15)
+            ->withQueryString();
         $departments = Employee::active()->distinct()->pluck('department');
 
         // Calculate payroll for each employee
