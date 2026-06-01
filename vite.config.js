f095bed19c978269b4d0935d93ffe6883e32221f
dreamerrri <114892885+dreamerrri@users.noreply.github.com>
2026-06-01 15:31:29 +0800

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/login.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/css/login.css', 'resources/js/app.js', 'resources/css/register.css'], //remove register pag sumabog
             refresh: true,
         }),
         tailwindcss(),
