a197fca131afed08d25e0006fbd565652d4c468c
Vincent Cruz <vincentcruz54@gmail.com>
2026-07-21 15:00:15 +0800

    ADDED WORK REQUEST FOR EMPLOYEES

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -184,6 +184,12 @@
                             <span class="overlay-minified:hidden">Attendance</span>
                         </a>
                     </li>
+                    <li>
+                        <a href="{{ route('work-requests.index') }}" class="{{ request()->routeIs('work-requests.*') ? 'active' : '' }}">
+                            <span class="icon-[tabler--notes] size-5"></span>
+                            <span class="overlay-minified:hidden">Work Requests</span>
+                        </a>
+                    </li>
                 @endif
             </ul>
         </div>
